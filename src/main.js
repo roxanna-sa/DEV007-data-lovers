8e0dc44fef23fb2352a5ae68357f3a4e2c76a517
Roxana Soto <roxanna.sa@gmail.com>
2023-05-02 12:41:35 -0400

    Inicio

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/got/got.js";
+console.log(data);
 
-console.log(example, data);
+const plantilla = document.querySelector("[name='plantilla']").outerHTML;
+document.querySelector("[name='plantilla']").style.display = "none";
+
+data.got.forEach((item, index)=>{
+  
+  let plantillaActual = plantilla;
+
+  plantillaActual = plantillaActual.replace("[Foto-Personaje]", item.imageUrl);
+  plantillaActual = plantillaActual.replace("[PrimerNombre-Personaje]", item.firstName);
+  plantillaActual = plantillaActual.replace("[Apellido-Personaje]", item.lastName);
+  plantillaActual = plantillaActual.replace("[Nombre-Personaje]", item.fullName);
+  plantillaActual = plantillaActual.replace("[Titulo-Personaje]", item.title);
+  
+
+// es lo mismo que el if
+  plantillaActual = plantillaActual.replace("[Casa-Personaje]", (item.family == "" ? "-" : item.family)); // ?= entonces : = si no (lo hago de esta forma por si hay campos vacíos)
+  
+  document.querySelector("#root").innerHTML +=  plantillaActual;
+});
