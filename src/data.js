a3391942b465a3b95b264b38ab3d007735deb547
mfrb15 <129767020+mfrb15@users.noreply.github.com>
2023-05-04 16:54:12 -0400

    Lista de selección

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const filtrarDatosStark = (data) => {
+//   const filtrar = data.got.filter(casaStark => casaStark.family === "House Stark" || casaStark.family === "Stark")
+//  return filtrar;
+// }  
+
+// export const filtrarDatosTargaryen = (data) => {
+//   const filtrar = data.got.filter(casaTargaryen => casaTargaryen.family === "House Targaryen"|| casaTargaryen.family === "Targaryan")
+//  return filtrar;
+// }  
+
+// export const filtrarDatosTarly = (data) => {
+//   const filtrar = data.got.filter(casaTarly => casaTarly.family === "House Tarly")
+//  return filtrar;
+// }  
+
+// add event listener click, 
+export const filtrarFamilias = (data, selecciondeCasa) => {
+  const filtrar = data.got.filter(casa => casa.family.includes(selecciondeCasa))
+ return filtrar;
+}  
+
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import data from "./data/got/got.js";
-console.log(data);
+console.log(data, "soy ese console");
+import * as datos from "./data.js";
+
 
 const plantilla = document.querySelector("[name='plantilla']").outerHTML;
 document.querySelector("[name='plantilla']").style.display = "none";
 
-data.got.forEach((item, index)=>{
-  
+data.got.forEach((item, index) => {
+
   let plantillaActual = plantilla;
 
   plantillaActual = plantillaActual.replace("[Foto-Personaje]", item.imageUrl);
@@ -16,10 +18,28 @@ data.got.forEach((item, index)=>{
   plantillaActual = plantillaActual.replace("[Fecha-Nacimiento]", item.born);
   plantillaActual = plantillaActual.replace("[Fecha-Muerte]", item.death);
 
-  
 
-// es lo mismo que el if
+
+  // es lo mismo que el if
   plantillaActual = plantillaActual.replace("[Casa-Personaje]", (item.family == "" ? "-" : item.family)); // ?= entonces : = si no (lo hago de esta forma por si hay campos vacíos)
-  
-  document.querySelector("#root").innerHTML +=  plantillaActual;
+
+  document.querySelector("#root").innerHTML += plantillaActual;
 });
+
+let listaDesplegable = document.getElementById('Familias');
+let seleccion = listaDesplegable.options[listaDesplegable.selectedIndex].value;
+console.log(seleccion); 
+
+
+
+
+
+
+//trabajar desde esta línea
+
+// console.log (datos.filtrarDatosStark(data), "filtro Casa Stark");
+// console.log (datos.filtrarDatosTargaryen(data), "filtro Casa Targaryen");
+// console.log (datos.filtrarDatosTarly(data), "filtro Casa Tarly")
+
+
+//Llamar elementos de lista desplegable 
