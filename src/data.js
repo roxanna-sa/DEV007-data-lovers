d84d22fadf863671cee48702a138c47f600706a4
Roxana Soto <roxanna.sa@gmail.com>
2023-05-04 16:55:17 -0400

    Cambios no estables

--- src/data.js
+++ src/data.js
@@ -1,9 +1,52 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
+};*/
+
+//listaPersonajes= c/pj  orden=ascendente/descendente
+/**
+ * recibe un arreglo y lo ordena en base al parámetro orden
+ * @param {Array} listaPersonajes Un arreglo con el formato de got.js
+ * @param {string} orden El texto "ascendente" o "descendente"
+ * @returns 
+ */
+export const ordenAlfabetico = (listaPersonajes,orden) => {
+  listaPersonajes.sort(orderByFullName);
+  if (orden !== "ascendente"){
+    return listaPersonajes.reverse();
+  }else{
+    return listaPersonajes;
+  }
 };
+
+function orderByFullName(a,b){
+  if (a.fullName > b.fullName){
+    return 1;
+  }else if(b.fullName > a.fullName){
+    return -1;
+  }else{
+    return 0;
+  }
+}
+
+
+  
+export function filtro() {
+  const casaSeleccionada = document.querySelector("#idSelect").value;
+
+  if (casaSeleccionada === ""){
+   dibujarPersonajes(data.got);
+   
+  }else{
+    const filtradosPorCasa = filtradosPorCasa(structureClone(data.got), casaSeleccionada); 
+
+   dibujarPersonajes(filtradosPorCasa);
+  }
+};
+
+console.log(filtro);
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
-import { example, anotherExample } from '../src/data.js';
+import { ordenAlfabetico } from '../src/data.js';
 
-
+/*
 describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
@@ -19,5 +19,19 @@ describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
+  });
+});*/
+
+describe("ordenAlfabetico", ()=> {
+  it("es una funcion", ()=> {
+    expect(typeof ordenAlfabetico).toBe("function"); //toBe es para datos primitivos
+  });
+
+  it("arreglo ordenado ascendente", ()=>{
+    expect(ordenAlfabetico(["C","A","D","B"],"ascendente")).toStrictEqual(["A","B","C","D"]); //toStrictEqual es para objetos completos 
+  });
+
+  it("arreglo ordenado descendente", ()=>{
+    expect(ordenAlfabetico(["C","A","D","B"],"descendente")).toStrictEqual(["D","C","B","A"]);
   });
 });
