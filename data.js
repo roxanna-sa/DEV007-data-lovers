ac80c5f85de398ad4f0af331b2334a991ecdd874
Roxana Soto <roxanna.sa@gmail.com>
2023-05-23 18:21:52 -0400

    Updates

---
+++ data.js
@@ -0,0 +1,71 @@
+//listaPersonajes= c/pj  orden=ascendente/descendente
+
+/**
+ * 
+ * @param {*} filtrados elemento data.got
+ * @param {*} tipoDeOrden es un texto que debería ser ascendente o descendente
+ * @returns 
+ */
+export const ordenAlfabetico = (filtrados,tipoDeOrden) => {
+  filtrados.sort(orderByFullName);
+  if (tipoDeOrden !== "ascendente"){ //ascendente es el value del select order HTML
+    return filtrados.reverse();
+  }else{
+    return filtrados;
+  }
+};
+
+function orderByFullName(a,b){
+  if (a.fullName > b.fullName){
+    return 1; //si el primer elemento es mayor que el segundo (en orden alfabético)
+  }else if(b.fullName > a.fullName){
+    return -1; //si el segundo elemento es mayor lo mueve para la izq
+  }else{
+    return 0; //queda donde mismo
+  }
+}
+
+export const filtrarFamilias = (data, seleccionDeCasa) => {
+  const filtrar = data.got.filter(casa => casa.family.toLowerCase().includes(seleccionDeCasa))
+  /*
+  filter solo funciona sobre arreglos y pide como si fuese un foreach el nombre de c/elemento
+  en este caso, cada elemento (que nosotras nombramos "casa") representa un objeto de got.js
+  y filter se encarga de sólo filtrar los elementos que cumplan la condición 
+  la condición es que el objeto.family en minúsculas incluya la casa que seleccionó el usuario
+  seleccion de casa es el parámetro, en main están los argumentos que se le envían a la función 
+  */
+  return filtrar;
+};
+
+
+//input
+
+export const filtrarNombres= (data, Name) => {
+  const filtrar = data.filter(nombre => nombre.fullName.toLowerCase().includes(Name.toLowerCase()));
+  // console.log(filtrar, "Soy la casa Seleccionada");
+  return filtrar;
+}; 
+
+
+//Conteo de personajes por familia
+
+export function conteoPersonajesPorFamilia(listadoPersonajes) {
+  let listadoDeFamilias = listadoPersonajes.map(x => x.family); //de todos los personajes, se obtienen las familias. Estas pueden repetirse más de una vez.
+  //listadoDeFamilias = listadoDeFamilias.filter(function(familia, index) { //filter siempre pide función dentro
+  //return listadoDeFamilias.indexOf(familia) === index;
+  //}); //Vivie
+  listadoDeFamilias = listadoDeFamilias.filter((familia, index) => listadoDeFamilias.indexOf(familia) === index); //Vivie
+                                              
+  const cantidadPersonajesPorFamilia = []; //se va a guardar la cantidad de personajes por c/familia
+  listadoDeFamilias.forEach((familia) => {
+    const cantidadEncontradaFamilias = listadoPersonajes.filter(x => x.family.toLowerCase().includes(familia.toLowerCase())).length; //.lenght es para cuantos de c/familia encontró. 
+    cantidadPersonajesPorFamilia.push(cantidadEncontradaFamilias);
+  });
+  
+  return { //crea un objeto que tiene 2 listas como resultado, ya que return convencional sólo retorna un objeto que puede ser primitivo o complejo
+    "listadoDeFamilias": listadoDeFamilias,                      
+    "cantidadPersonajesPorFamilia": cantidadPersonajesPorFamilia
+  }
+}
+
+
---
+++ main.js
@@ -0,0 +1,144 @@
+import { filtrarFamilias, ordenAlfabetico, filtrarNombres, conteoPersonajesPorFamilia } from "./data.js";
+import data from "./data/got/got.js";
+
+
+const plantilla = document.querySelector("[name='plantilla']").outerHTML;
+document.querySelector("[name='plantilla']").style.display = "none";
+let familiaSeleccionada = "";
+let miembrosFamilia = {} //acá se va a guardar un objeto, por eso está vacío
+const mediaQMovil = window.matchMedia('(max-width: 500px)');
+const mediaQTablet = window.matchMedia('(max-width: 820px)');
+
+/**
+ * Esta función se encarga de dibujar la segunda columna
+ * @param {filtrados} listaPersonajes 
+ */
+function dibujarPersonajesPantalla(listaPersonajes) {
+  document.querySelector("#root").innerHTML = ""; //limpiar el root que es donde dibujamos cada personaje
+  listaPersonajes.forEach((item) => { //data.got.forEach((item, index)
+    let plantillaActual = plantilla;
+
+    plantillaActual = plantillaActual.replace("#", item.imageUrl); //pusimos # porque el [] da error en consola con la img
+    // plantillaActual = plantillaActual.replace("[PrimerNombre-Personaje]", item.firstName);
+    // plantillaActual = plantillaActual.replace("[Apellido-Personaje]", item.lastName);
+    plantillaActual = plantillaActual.replace("[Nombre-Personaje]", item.fullName);
+    // plantillaActual = plantillaActual.replace("[Titulo-Personaje]", item.title);
+    // plantillaActual = plantillaActual.replace("[Fecha-Nacimiento]", item.born);
+    // plantillaActual = plantillaActual.replace("[Fecha-Muerte]", item.death);
+    plantillaActual = plantillaActual.replace("[id]",item.id);
+    // es lo mismo que el if
+    // plantillaActual = plantillaActual.replace("[Casa-Personaje]", (item.family === "" ? "-" : item.family)); // ?= entonces : = si no (lo hago de esta forma por si hay campos vacíos)
+
+    document.querySelector("#root").innerHTML += plantillaActual;
+  });
+
+  Array.from(document.getElementsByClassName("botonPersonaje")).forEach(x => x.addEventListener("click",dibujarMiembrosPantalla));
+  
+}
+//Actualizado 15/mayo= Ya no la usamos porque no necesitamos mostrar todos los pjs
+//dibujarPersonajesPantalla(data.got); //le mando todos los personajes a la función que tiene el parámetro listaPersonajes (al cargar la página)
+function dibujarMiembrosPantalla(){
+  Array.from(document.getElementsByClassName("botonPersonaje")).forEach(x => x.classList.remove("seleccion-boton")); //quitar clase "seleccion-boton" a todos los botones que tengan la clase botón familia
+  this.classList.add("seleccion-boton"); //Agregar clase selección botón al botón que se le hace click
+
+  const ID = parseInt(this.id);
+  const personajeSeleccionado = data.got[ID];
+  
+  document.querySelector("#imgPersonajeSeleccionado").src = personajeSeleccionado.imageUrl;
+  document.querySelector("#nombrePersonajeSeleccionado").innerHTML = personajeSeleccionado.firstName;
+  document.querySelector("#apellidoPersonajeSeleccionado").innerHTML = personajeSeleccionado.lastName;
+  document.querySelector("#nombreCompletoPersonajeSeleccionado").innerHTML = personajeSeleccionado.fullName;
+  document.querySelector("#tituloPersonajeSeleccionado").innerHTML = personajeSeleccionado.title;
+  document.querySelector("#familiaPersonajeSeleccionado").innerHTML = personajeSeleccionado.family;
+  document.querySelector("#nacimientoPersonajeSeleccionado").innerHTML = personajeSeleccionado.born;
+  document.querySelector("#muertePersonajeSeleccionado").innerHTML = personajeSeleccionado.death;
+
+  document.querySelector("#columna3").style.display = "block";
+
+  if (mediaQMovil.matches || mediaQTablet.matches){
+    document.querySelector("#root").style.display = "none";
+  }
+}
+ 
+function filtro(){
+  let filtrados = structuredClone(data);
+
+  // Proceso 1: Filtro por familia
+
+  filtrados = filtrarFamilias(filtrados, familiaSeleccionada); //hace el filtro por familia y guarda esa lista ordenada en la variable filtrados
+  console.log(filtrados)
+
+
+
+  //Proceso 2: hacer orden AZ/ZA acá
+  const tipoDeOrden = document.getElementById('selectOrder').value;
+  if (tipoDeOrden !== "") {
+    filtrados = ordenAlfabetico(filtrados, tipoDeOrden);  //hace el orden alfabético y guarda esa lista ordenada en la variable filtrados
+  }
+
+  // Proceso 3: Hacer filtro por input de texto
+  const fullName = document.getElementById('inputSearch').value;
+  filtrados = filtrarNombres(filtrados, fullName);
+
+  // Proceso Final: Dibujar el resultado de los proceso en pantalla
+  dibujarPersonajesPantalla(filtrados);
+  
+}
+
+
+//vincular botones html a JS
+function seleccionarFamilia(){
+  Array.from(document.getElementsByClassName("botonFamilia")).forEach(x => x.classList.remove("seleccion-boton")); //quitar clase "seleccion-boton" a todos los botones que tengan la clase botón familia
+  this.classList.add("seleccion-boton"); //Agregar clase selección botón al botón que se le hace click
+  let seleccion = this.children[0].innerHTML.trim().toLowerCase(); //en [0] porque el hijo (<p> es el único elemento y está en la posición CERO)
+  if (seleccion === "all"){ //si la selección es "todos" se deja en vacío para que no filtre nada y me muestre todos los pjs
+    seleccion = "";
+  }
+  familiaSeleccionada = seleccion; //modificación variable global por la seleccion hecha
+
+  if (seleccion === ""){
+    document.getElementById("cantidadMiembros").innerHTML = "";
+  }else{
+
+    //Escribir cantidad de miembros por familia
+    const indiceEncontrado = miembrosFamilia.listadoDeFamilias.findIndex((item)=> item.toLowerCase().includes(seleccion));
+    const cantidadMiembros = miembrosFamilia.cantidadPersonajesPorFamilia [indiceEncontrado];
+    document.getElementById("cantidadMiembros").innerHTML = cantidadMiembros;
+  }
+  filtro(); //llamar a filtro que se encarga de filtrar y dibujar
+
+  //mostrar la segunda columna que es donde aparece el resultado del filtro
+
+  document.querySelector("#root").style.display = "block";
+  document.querySelector("#cantidadDeMiembros").style.display = "block";
+  document.querySelector("#selectOrder").style.display = "block";
+  
+  if (mediaQMovil.matches || mediaQTablet.matches){
+    document.querySelector("#columna1").style.display = "none";
+  }
+}
+
+
+
+/*function limpiarFiltroFamilia(){
+  Array.from(document.getElementsByClassName("botonFamilia")).forEach(x => x.classList.remove("seleccion-boton"));
+  document.querySelector("#root").style.display = "none";
+  document.querySelector("#cantidadDeMiembros").style.display = "none";
+  document.querySelector("#selectOrder").style.display = "none";
+  document.querySelector("#columna3").style.display = "none";
+  Se usó para borrar elementos al hacer click en el logo principal, pero se decidió utilizar una etiqueta <a> en HTML para recargar el sitio 
+}*/
+
+
+
+document.querySelector("#selectOrder").addEventListener("change",filtro);
+document.querySelector("#inputSearch").addEventListener("keyup", filtro);
+Array.from(document.getElementsByClassName("botonFamilia")).forEach(x => x.addEventListener("click",seleccionarFamilia));
+/*document.querySelector("#logo-principal").addEventListener("click",limpiarFiltroFamilia);*/
+
+
+miembrosFamilia = conteoPersonajesPorFamilia(structuredClone(data.got));
+
+//PARA SACAR EL VALOR DE UNA CAJA DE TEXTO GET ELEMENTBYID().VALUE
+
+
